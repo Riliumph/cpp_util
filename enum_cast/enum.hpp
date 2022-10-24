c6ea9f7bf6c7e5edb977a30312c7bb9fbb90853c
Riliumph <riliumph@outlook.jp>
2022-10-25 03:33:11 +0900

    format by Mozilla rule

--- enum_cast/enum.hpp
+++ enum_cast/enum.hpp
@@ -1,6 +1,9 @@
+// STL
 #include <type_traits>
-template <typename E>
-constexpr auto e_cast(E e) -> typename std::underlying_type<E>::type
+
+template<typename E>
+constexpr auto
+e_cast(E e) -> typename std::underlying_type<E>::type
 {
-    return static_cast<typename std::underlying_type<E>::type>(e);
+  return static_cast<typename std::underlying_type<E>::type>(e);
 }
