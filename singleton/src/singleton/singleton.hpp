0cf331258bad0df542d425b8915a857bd3b72fd8
riliumph <riliumph@gmail.com>
2022-12-14 05:20:21 +0900

    [update] Remove finalizer by using smart pointer

--- singleton/src/singleton/singleton.hpp
+++ singleton/src/singleton/singleton.hpp
@@ -2,9 +2,8 @@
 #define INCLUDE_SINGLETON_SINGLETON_HPP
 // STL
 #include <cassert>
+#include <memory>
 #include <mutex>
-// original
-#include "finalizer.hpp"
 
 template<typename T>
 class Singleton final
@@ -18,24 +17,14 @@ public:
   }
 
 private:
-  static void create()
-  {
-    instance = new T;
-    singleton::Finalizer::Add(&Singleton<T>::destroy);
-  }
-
-  static void destroy()
-  {
-    delete instance;
-    instance = nullptr;
-  }
+  static void create() { instance = std::make_unique<T>(); }
 
   static std::once_flag is_initialized;
-  static T* instance;
+  static std::unique_ptr<T> instance;
 };
 
 template<typename T>
 std::once_flag Singleton<T>::is_initialized;
 template<typename T>
-T* Singleton<T>::instance = nullptr;
+std::unique_ptr<T> Singleton<T>::instance;
 #endif // INCLUDE_SINGLETON_SINGLETON_HPP
