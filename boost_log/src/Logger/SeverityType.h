dfee94f365623dcefe1bee9bc8b550a622352144
Riliumph <riliumph@outlook.jp>
2023-08-18 23:05:26 +0900

    [update] build by dev container
    fix build error
    format code
    add document

--- boost_log/src/Logger/SeverityType.h
+++ boost_log/src/Logger/SeverityType.h
@@ -3,27 +3,26 @@
 /* Boost */
 #include <boost/log/expressions/keyword.hpp> // for BOOST_LOG_ATTRIBUTE_KEYWORD
 
-namespace Log
+namespace Log {
+/// Log level type
+typedef enum struct SEVERITY_TYPE
 {
-	/// Log level type
-	typedef enum struct SEVERITY_TYPE
-	{
-		/*note,*/ Debug,
-		Info,
-		Warn,
-		Error,
-		/*critical,*/ Fatal,
-		NUM
-	} Lv;
+  /*note,*/ Debug,
+  Info,
+  Warn,
+  Error,
+  /*critical,*/ Fatal,
+  NUM
+} Lv;
 
-	/**
-	 * Declare keyword
-	 *
-	 * define struct severity
-	 * @param severity [i] declare struct name
-	 * @param "severity" [i] struct attribute name
-	 * @param Log::Lv [i] instance tag
-	 */
-	BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity", Log::Lv);
+/**
+ * Declare keyword
+ *
+ * define struct severity
+ * @param severity [i] declare struct name
+ * @param "severity" [i] struct attribute name
+ * @param Log::Lv [i] instance tag
+ */
+BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity", Log::Lv);
 } // namespace Log
 #endif // INCLUDE_SEVERITY_TYPE_H
--- boost_log/src/Logger/System.h
+++ boost_log/src/Logger/System.h
@@ -1,22 +1,23 @@
 #ifndef INCLUDE_LOGGER_SYSTEM_H
 #define INCLUDE_LOGGER_SYSTEM_H
 /* Boost */
-#include <boost/log/sources/severity_feature.hpp>		// for BOOST_LOG_SEV
+#include <boost/log/sources/severity_feature.hpp>       // for BOOST_LOG_SEV
 #include <boost/log/utility/manipulators/add_value.hpp> // for add_value
 /* Original */
 #include "GlobalLogger.h"
 
-#define LOG(sv) BOOST_LOG_SEV(Log::g_logger::get(), sv)        \
-					<< boost::log::add_value("Line", __LINE__) \
-					<< boost::log::add_value("File", __FILE__) \
-					<< boost::log::add_value("Func", __func__)
+#define LOG(sv)                                                                \
+  BOOST_LOG_SEV(Log::g_logger::get(), sv)                                      \
+    << boost::log::add_value("Line", __LINE__)                                 \
+    << boost::log::add_value("File", __FILE__)                                 \
+    << boost::log::add_value("Func", __func__)
 
-namespace Log
-{
-	/**
-	 * Boot up log system
-	 * reflect log config after this method execution
-	 */
-	void BootLogSystem();
+namespace Log {
+/**
+ * Boot up log system
+ * reflect log config after this method execution
+ */
+void
+BootLogSystem();
 } // namespace Log
 #endif // INCLUDE_LOGGER_SYSTEM_H
