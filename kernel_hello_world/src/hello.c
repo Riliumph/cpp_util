3be3b34d4caceb1d812188c2b164b786a90145e5
Riliumph <riliumph@outlook.jp>
2023-08-18 23:07:49 +0900

    [update] build by dev container (not fix build error)

--- kernel_hello_world/src/hello.c
+++ kernel_hello_world/src/hello.c
@@ -1,20 +1,22 @@
-#include <linux>
+#include <linux/kernel.h>
+#include <linux/module.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
-static int
+static int __init
 hello_init(void)
 {
   printk(KERN_ALERT "driver loaded\n");
-  printk(KERN_ALERT "hello world\n");
+  printk(KERN_INFO "hello world\n");
   return 0;
 }
 
-static void
+static void __exit
 hello_exit(void)
 {
+  printk(KERN_INFO "goodbye world\n");
   printk(KERN_ALERT "driver unloaded\n");
 }
 
 module_init(hello_init);
-module_exit(hello\_exit);
+module_exit(hello_exit);
