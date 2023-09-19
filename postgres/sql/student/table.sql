10162ac12d2fc46efbd1a8ce769881f2262a1f7d
Riliumph <riliumph@outlook.jp>
2023-09-20 07:26:18 +0900

    [update] sample sql

--- postgres/sql/student/table.sql
+++ postgres/sql/student/table.sql
@@ -1,7 +1,20 @@
 create table student (
-  id int primary key,
-  name text,
-  age int,
-  updated_at timestamp with time zone,
-  created_at timestamp with time zone
+  id serial not null primary key,
+  name text not null,
+  age int not null,
+  updated_at timestamptz default now(),
+  created_at timestamptz default now()
 );
+
+SELECT
+  CASE
+    WHEN (
+      SELECT
+        age
+      FROM
+        student
+      WHERE
+        id = 1
+    ) = 10 THEN true
+    ELSE false
+  END;
