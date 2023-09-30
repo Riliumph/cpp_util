caa3b73fe2c8164a9800f505e9d0b6523736e972
Riliumph <riliumph@outlook.jp>
2023-09-30 23:12:18 +0000

    [update] automatically init postgres
    use docker-entrypoint

--- postgres/sql/db.sql
+++
@@ -1,5 +0,0 @@
-create database sample_db;
-create table sample_table (id int, name text, age int);
-insert into sample_table values (1, 'sample1', 1);
-insert into sample_table values (2, 'sample2', 2);
-insert into sample_table values (3, 'sample3', 3);
