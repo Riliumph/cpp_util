4d5fe80b4e5ff4840bf7acdee653a72fd372a7b7
Riliumph <riliumph@outlook.jp>
2024-05-12 21:00:31 +0000

    [update] new test item

--- chrono/src/chrono/sleep_test.cpp
+++ chrono/src/chrono/sleep_test.cpp
@@ -83,3 +83,40 @@ TEST(fps_sleep, fps30_msec)
     EXPECT_EQ(t.o.expected, actual);
   }
 }
+
+TEST(fps_sleep, fps60_usec)
+{
+  struct test_data
+  {
+    system_clock::time_point start;
+    system_clock::time_point end;
+  };
+  struct test_result
+  {
+    usecs expected;
+  };
+  struct test_set
+  {
+    const std::string name;
+    struct test_data i;
+    struct test_result o;
+  };
+
+  auto now = time_point_cast<usecs>(system_clock::now());
+  struct test_set tt[] = {
+    {
+      "待機するケース",
+      {
+        now,
+        now + 10ms,
+      },
+      {
+        6666us,
+      },
+    },
+  };
+  for (const auto& t : tt) {
+    auto actual = fps_sleep<60, usecs>(t.i.start, t.i.end);
+    EXPECT_EQ(t.o.expected, actual);
+  }
+}
