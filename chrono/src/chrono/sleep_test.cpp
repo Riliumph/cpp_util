f6c70a951c178c91bd7673fbad6ed61f193c06d0
Riliumph <riliumph@outlook.jp>
2024-05-12 19:05:03 +0000

    [add] test fps_sleep

---
+++ chrono/src/chrono/sleep_test.cpp
@@ -0,0 +1,85 @@
+#include "sleep.hpp"
+// STL
+#include <ctime>
+// 3rd
+#include <gtest/gtest.h>
+// original
+#include "name.hpp"
+#include "operator.hpp"
+
+using namespace std::chrono;
+using namespace std::literals::chrono_literals;
+
+TEST(fps_sleep, fps60_msec)
+{
+  struct test_data
+  {
+    system_clock::time_point start;
+    system_clock::time_point end;
+  };
+  struct test_result
+  {
+    msecs expected;
+  };
+  struct test_set
+  {
+    const std::string name;
+    struct test_data i;
+    struct test_result o;
+  };
+
+  auto now = time_point_cast<msecs>(system_clock::now());
+  struct test_set tt[] = {
+    {
+      "待機するケース",
+      {
+        now,
+        now + 10ms,
+      },
+      {
+        6ms,
+      },
+    },
+  };
+  for (const auto& t : tt) {
+    auto actual = fps_sleep<60, msecs>(t.i.start, t.i.end);
+    EXPECT_EQ(t.o.expected, actual);
+  }
+}
+
+TEST(fps_sleep, fps30_msec)
+{
+  struct test_data
+  {
+    system_clock::time_point start;
+    system_clock::time_point end;
+  };
+  struct test_result
+  {
+    msecs expected;
+  };
+  struct test_set
+  {
+    const std::string name;
+    struct test_data i;
+    struct test_result o;
+  };
+
+  auto now = time_point_cast<msecs>(system_clock::now());
+  struct test_set tt[] = {
+    {
+      "待機するケース",
+      {
+        now,
+        now + 10ms,
+      },
+      {
+        23ms,
+      },
+    },
+  };
+  for (const auto& t : tt) {
+    auto actual = fps_sleep<30, msecs>(t.i.start, t.i.end);
+    EXPECT_EQ(t.o.expected, actual);
+  }
+}
