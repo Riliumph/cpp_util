2c4570eae69094609a7d0945769fdcc3597d7e4d
Riliumph <riliumph@outlook.jp>
2024-05-12 16:45:37 +0000

    [add] std::chrono::duration supports std::ostream

--- chrono/src/chrono/operator.hpp
+++ chrono/src/chrono/operator.hpp
@@ -6,4 +6,33 @@
 std::ostream&
 operator<<(std::ostream&, const std::chrono::system_clock::time_point&);
 
+template<typename Rep, typename Period>
+std::ostream&
+operator<<(std::ostream& os, const std::chrono::duration<Rep, Period>& lv)
+{
+  using namespace std::chrono;
+  using nsecs = std::chrono::nanoseconds;
+  using usecs = std::chrono::microseconds;
+  using msecs = std::chrono::milliseconds;
+  using secs = std::chrono::seconds;
+  using mins = std::chrono::minutes;
+  using hours = std::chrono::hours;
+
+  if constexpr (std::ratio_equal_v<Period, nsecs::period>) {
+    os << lv.count() << "ns";
+  } else if constexpr (std::ratio_equal_v<Period, usecs::period>) {
+    os << lv.count() << "us";
+  } else if constexpr (std::ratio_equal_v<Period, msecs::period>) {
+    os << lv.count() << "ms";
+  } else if constexpr (std::ratio_equal_v<Period, secs::period>) {
+    os << lv.count() << "s";
+  } else if constexpr (std::ratio_equal_v<Period, mins::period>) {
+    os << lv.count() << "min";
+  } else if constexpr (std::ratio_equal_v<Period, hours::period>) {
+    os << lv.count() << "h";
+  } else {
+    os << lv.count() << "unknown";
+  }
+  return os;
+}
 #endif // INCLUDE_CHRONO_OPERATOR_HPP
