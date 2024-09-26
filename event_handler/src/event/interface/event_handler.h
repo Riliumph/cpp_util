1bd64415b69462a6837f355a3f95a05d4c967191
Riliumph <riliumph@outlook.jp>
2024-09-26 18:21:08 +0000

    [update] FDに対してイベントを設定できる機能を追加

--- event_handler/src/event/interface/event_handler.h
+++ event_handler/src/event/interface/event_handler.h
@@ -3,18 +3,21 @@
 // STL
 #include <chrono>
 #include <functional>
+#include <optional>
 
 namespace event::IF {
 /// @brief イベントハンドラーとしてのIFクラス
 class EventHandler
 {
 public:
+  using event_func = std::function<void(int)>;
+
   virtual bool CanReady() = 0;
-  virtual int RegisterEvent(int, int) = 0;
-  virtual int ModifyEvent(int, int) = 0;
+  virtual int RegisterEvent(int, int, event_func) = 0;
+  virtual int ModifyEvent(int, int, std::optional<event_func>) = 0;
   virtual int DeleteEvent(int, int) = 0;
 
-  virtual void LoopEvent(std::function<bool(int)>) = 0;
+  virtual void LoopEvent() = 0;
   virtual void Timeout(std::chrono::milliseconds to) = 0;
 
 protected:
