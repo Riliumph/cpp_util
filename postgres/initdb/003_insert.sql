aaa1b10cb749d683b1cb2a2638f80b19ae4f78ff
Riliumph <riliumph@outlook.jp>
2024-12-03 08:46:17 +0000

    [update] SQL

--- postgres/initdb/003_insert.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO student (name, age) VALUES ('student1', 17);
-INSERT INTO student (name, age) VALUES ('student2', 18);
-INSERT INTO student (name, age) VALUES ('student3', 17);
---
+++ postgres/initdb/102_insert_student.sql
@@ -0,0 +1,6 @@
+INSERT INTO
+  students (name, age)
+VALUES
+  ('student1', 17),
+  ('student2', 18),
+  ('student3', 17);
