9a280942213a7c01bd62064a5d705207708a1705
Riliumph <riliumph@outlook.jp>
2025-01-20 18:00:35 +0000

    [Update] クライアントのイベントを登録する処理を追加
    以下のコールバックをするように設定
    EPOLLINイベントの際に登録されたコールバックを実行
    EPOLLRDHUPイベントの際に切断処理のコールバックを実行

--- socket/src/tcp_server.cpp
+++ socket/src/tcp_server.cpp
@@ -18,8 +18,59 @@ main()
   hint.ai_socktype = SOCK_STREAM;
   hint.ai_flags = AI_PASSIVE;
   auto eh = std::make_shared<event::EpollHandler>();
+  eh->Timeout(std::nullopt);
   std::cout << "create server" << std::endl;
   nw::ipv4::tcp::Server srv(eh, port_no, hint);
+  srv.Event([](int fd) {
+    char buffer[1024];
+    auto recv_size = read(fd, buffer, sizeof(buffer));
+    std::cout << std::string(buffer) << std::endl;
+      // std::array<uint8_t, 12> buffer;
+      // std::cout << "recv" << std::endl;
+      // auto recv_size = read(fd, buffer.data(), buffer.size());
+      // if (recv_size < 0) {
+      //   return;
+      // } else if (recv_size == 0) {
+      //   std::cout << "receive EOS." << std::endl;
+      //   return;
+      // } else if (0 < recv_size && recv_size < (ssize_t)buffer.size()) {
+      //   std::cout << "incomplete receive" << std::endl;
+      //   return;
+      // }
+
+      // int32_t raw1, raw2;
+      // int16_t raw3, raw4;
+      // int start_point = 0;
+      // std::memcpy(&raw1, buffer.data() + start_point, sizeof(raw1));
+      // start_point += sizeof(raw1);
+      // std::memcpy(&raw2, buffer.data() + start_point, sizeof(raw2));
+      // start_point += sizeof(raw2);
+      // std::memcpy(&raw3, buffer.data() + start_point, sizeof(raw3));
+      // start_point += sizeof(raw3);
+      // std::memcpy(&raw4, buffer.data() + start_point, sizeof(raw4));
+
+      // endian::bytes raw1_b;
+      // raw1_b.byte4 = raw1;
+
+      // auto data1 = endian::Reverse(raw1);
+      // auto data2 = endian::Reverse(raw2);
+      // auto data3 = endian::Reverse(raw3);
+      // auto data4 = endian::Reverse(raw4);
+
+      // endian::bytes raw1_b_;
+      // raw1_b_.byte4 = data1;
+
+      // std::printf("1:%+10d(0x%08x)\n->%+10d(0x%08x)\n", raw1, raw1, data1,
+      // data1); std::printf("2:%+10d(0x%08x)\n->%+10d(0x%08x)\n", raw2, raw2,
+      // data2, data2); std::printf("3:%+10d(0x%08x)\n->%+10d(0x%08x)\n", raw3,
+      // raw3, data3, data3); std::printf("4:%+10d(0x%08x)\n->%+10d(0x%08x)\n",
+      // raw4, raw4, data4, data4);
+
+      // std::cout << "raw1_b\n" << raw1_b << std::endl;
+      // std::cout << "raw1_b_\n" << raw1_b_ << std::endl;
+
+      return;
+  });
   std::cout << "establish server" << std::endl;
   if (srv.Establish() < 0) {
     std::cerr << "failed to establish server" << std::endl;
@@ -27,50 +78,5 @@ main()
   }
 
   std::cout << "start server" << std::endl;
-  srv.Start([](int fd) {
-    std::array<uint8_t, 12> buffer;
-    std::cout << "recv" << std::endl;
-    auto recv_size = read(fd, buffer.data(), buffer.size());
-    if (recv_size < 0) {
-      return false;
-    } else if (recv_size == 0) {
-      std::cout << "receive EOS." << std::endl;
-      return false;
-    } else if (0 < recv_size && recv_size < (ssize_t)buffer.size()) {
-      std::cout << "incomplete receive" << std::endl;
-      return false;
-    }
-
-    int32_t raw1, raw2;
-    int16_t raw3, raw4;
-    int start_point = 0;
-    std::memcpy(&raw1, buffer.data() + start_point, sizeof(raw1));
-    start_point += sizeof(raw1);
-    std::memcpy(&raw2, buffer.data() + start_point, sizeof(raw2));
-    start_point += sizeof(raw2);
-    std::memcpy(&raw3, buffer.data() + start_point, sizeof(raw3));
-    start_point += sizeof(raw3);
-    std::memcpy(&raw4, buffer.data() + start_point, sizeof(raw4));
-
-    endian::bytes raw1_b;
-    raw1_b.byte4 = raw1;
-
-    auto data1 = endian::Reverse(raw1);
-    auto data2 = endian::Reverse(raw2);
-    auto data3 = endian::Reverse(raw3);
-    auto data4 = endian::Reverse(raw4);
-
-    endian::bytes raw1_b_;
-    raw1_b_.byte4 = data1;
-
-    std::printf("1:%+10d(0x%08x)\n->%+10d(0x%08x)\n", raw1, raw1, data1, data1);
-    std::printf("2:%+10d(0x%08x)\n->%+10d(0x%08x)\n", raw2, raw2, data2, data2);
-    std::printf("3:%+10d(0x%08x)\n->%+10d(0x%08x)\n", raw3, raw3, data3, data3);
-    std::printf("4:%+10d(0x%08x)\n->%+10d(0x%08x)\n", raw4, raw4, data4, data4);
-
-    std::cout << "raw1_b\n" << raw1_b << std::endl;
-    std::cout << "raw1_b_\n" << raw1_b_ << std::endl;
-
-    return true;
-  });
+  srv.Start();
 }
