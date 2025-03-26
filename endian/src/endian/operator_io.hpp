ec1c85f655da06da5ff7412d0a01bc75c83a495b
Riliumph <riliumph@outlook.jp>
2025-03-26 22:38:42 +0000

    [update] endian module
    テストケースを追加
    サンプルmainを更新

--- endian/src/endian/operator_io.hpp
+++ endian/src/endian/operator_io.hpp
@@ -9,9 +9,9 @@ namespace endian {
 std::ostream&
 operator<<(std::ostream& os, bytes lv)
 {
-  std::printf("byte(4): %08X\n", lv.byte4);
-  std::printf("byte(2): %08X, %08X\n", lv.byte2[0], lv.byte2[1]);
-  std::printf("byte(1): %08X, %08X, %08X, %08X\n",
+  std::printf("byte(4): 0x%08X\n", lv.byte4);
+  std::printf("byte(2): 0x%04X, 0x%04X\n", lv.byte2[0], lv.byte2[1]);
+  std::printf("byte(1): 0x%02X, 0x%02X, 0x%02X, 0x%02X\n",
               lv.byte1[0],
               lv.byte1[1],
               lv.byte1[2],
--- endian/src/main.cpp
+++ endian/src/main.cpp
@@ -9,6 +9,19 @@ main(void)
 {
   endian::bytes b;
   b.byte4 = 0x12345678;
+  std::cout << b.byte4 << std::endl;
+  std::cout << b << std::endl;
+
+  b.byte4 = 0;
+  b.byte1[0] = 'a'; // 61
+  b.byte1[1] = 'A'; // 41
+  std::cout << b.byte4 << std::endl;
+  std::cout << b << std::endl;
+
+  b.byte4 = 0;
+  b.byte1[0] = 0xff; // 61
+  b.byte1[1] = 0xdd; // 41
+  std::cout << b.byte4 << std::endl;
   std::cout << b << std::endl;
   return 0;
 }
