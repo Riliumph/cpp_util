7c46783dbf14825c0a8cd2e944a87ef59467d305
Riliumph <riliumph@outlook.jp>
2025-04-23 17:08:15 +0000

    [update] 抽象基底クラスを追加
    IFクラスからEvent関数を削除
    ABCクラスにEvent関数を移動

--- socket/src/network/ipv4.h
+++ socket/src/network/ipv4.h
@@ -1,5 +1,6 @@
 #ifndef INCLUDE_IPV4_H
 #define INCLUDE_IPV4_H
+#include "ipv4/abc/socket_server.h"
 #include "ipv4/factory.h"
 #include "ipv4/tcp.h"
 #include "ipv4/udp.h"
---
+++ socket/src/network/ipv4/abc/socket_server.h
@@ -0,0 +1,23 @@
+#ifndef INCLUDE_NETWORK_IPV4_ABC_SOCKET_SERVER_H
+#define INCLUDE_NETWORK_IPV4_ABC_SOCKET_SERVER_H
+// super class
+#include "network/interface/server.h"
+// Original Lib
+#include <event.h>
+
+namespace nw {
+namespace ipv4 {
+using event_handler_t = event::IF::EventHandler;
+using event_callback_t = event_handler_t::callback;
+namespace abc {
+class SocketServer : public nw::IF::Server
+{
+public: // IF
+  virtual int Establish() = 0;
+  virtual bool Start() = 0;
+  virtual void Event(event_callback_t) = 0;
+};
+} // namespace abc
+} // ipv4
+} // nw
+#endif // INCLUDE_NETWORK_IPV4_ABC_SOCKET_SERVER_H
