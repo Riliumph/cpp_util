2d1c8bfb734821036005a36d793c95b87e722293
Riliumph <riliumph@outlook.jp>
2025-06-21 19:52:22 +0000

    [delete] テスト化されているコードをサンプルから削除

--- std_ext/src/main.cpp
+++ std_ext/src/main.cpp
@@ -31,22 +31,5 @@ main(void)
 
   a[0]["id"] = a[0]["id"] * 2;
   std::cout << a[0]["id"] << std::endl;
-  // bit
-  std_ext::bit::bytes b;
-  b.byte4 = 0x12345678;
-  std::cout << b.byte4 << std::endl;
-  std::cout << b << std::endl;
-
-  b.byte4 = 0;
-  b.byte1[0] = 'a'; // 61
-  b.byte1[1] = 'A'; // 41
-  std::cout << b.byte4 << std::endl;
-  std::cout << b << std::endl;
-
-  b.byte4 = 0;
-  b.byte1[0] = 0xff; // 61
-  b.byte1[1] = 0xdd; // 41
-  std::cout << b.byte4 << std::endl;
-  std::cout << b << std::endl;
   return 0;
 }
